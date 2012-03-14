bc7fdd548cf990527fb77ef8e029bbada6585eae
twidi <s.angel@twidi.com>
2012-03-14 23:55:22 +0100

    Autofill slugs from names in admin

--- source/fpe2aa/admin.py
+++ source/fpe2aa/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 
 from fpe2aa.models import Platform, AppType, Author, Application
 
-admin.site.register(Platform)
-admin.site.register(AppType)
-admin.site.register(Author)
-admin.site.register(Application)
+class BaseAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class PlatformAdmin(BaseAdmin):
+    pass
+
+class AppTypeAdmin(BaseAdmin):
+    pass
+
+class AuthorAdmin(BaseAdmin):
+    pass
+
+class ApplicationAdmin(BaseAdmin):
+    pass
+
+admin.site.register(Platform, PlatformAdmin)
+admin.site.register(AppType, AppTypeAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Application, ApplicationAdmin)
