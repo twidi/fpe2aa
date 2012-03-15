c374b387a952548a5a6c9262491983015c18921c
twidi <s.angel@twidi.com>
2012-03-15 01:00:04 +0100

    Add links between pages

--- source/fpe2aa/urls.py
+++ source/fpe2aa/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^type/$', ListView.as_view(model=AppType), name='type_list'),
     url(r'^type/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=AppType), name='type_detail'),
 
-    url(r'^plateforme/$', ListView.as_view(model=Platform), name='plateform_list'),
+    url(r'^plateforme/$', ListView.as_view(model=Platform), name='platform_list'),
     url(r'^plateforme/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Platform), name='platform_detail'),
 
     url(r'^auteur/$', ListView.as_view(model=Author), name='author_list'),
