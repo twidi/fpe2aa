89087c6a41d1d493b1868d34e211b9670a188570
twidi <s.angel@twidi.com>
2012-03-19 00:11:53 +0100

    Remove some useless pages (list of authors, platformes and types)

--- source/fpe2aa/urls.py
+++ source/fpe2aa/urls.py
@@ -3,15 +3,9 @@ from django.views.generic import ListView, DetailView
 from fpe2aa.models import AppType, Platform, Author, Application
 
 urlpatterns = patterns('',
-    url(r'^types/$', ListView.as_view(model=AppType), name='type_list'),
     url(r'^type/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=AppType), name='type_detail'),
-
-    url(r'^plateformes/$', ListView.as_view(model=Platform), name='platform_list'),
     url(r'^plateforme/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Platform), name='platform_detail'),
-
-    url(r'^auteurs/$', ListView.as_view(model=Author), name='author_list'),
     url(r'^auteur/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Author), name='author_detail'),
-
     url(r'^applications/$', ListView.as_view(model=Application), name='application_list'),
     url(r'^(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Application), name='application_detail'),
 )
