4ecaf73c63957fc01810df912f28a3d877869f0d
twidi <s.angel@twidi.com>
2012-03-19 02:03:32 +0100

    Add an about page

--- source/project/urls.py
+++ source/project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.views.generic import ListView
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,6 +12,7 @@ from fpe2aa.models import Application
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Application, template_name='home.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^a-propos/', direct_to_template, { 'template': 'about.html', 'extra_context': dict(section='about') }, name="about"),
     url(r'^', include('fpe2aa.urls')),
 )
 
