5baf09e06e516d446fba02b97d6574049f9faed0
twidi <s.angel@twidi.com>
2012-03-20 23:40:14 +0100

    Add an event on analytics when clicking on an application. cf #6

--- source/project/static/js/script.js
+++ source/project/static/js/script.js
@@ -1,7 +1,15 @@
-/* Author:
-
-*/
-
+if (ANALYTICS_CODE) {
+    $(document).ready(function(){
+        $('a.app-link').click(function() {
+            var link = $(this),
+                app = link.data('app');
+            if (!app) { return true; }
+            if (typeof(_gaq) != 'undefined') {
+                _gaq.push(['_trackEvent', 'Applications', 'click', app]);
+            }
+        });
+    });
+}
 
 
 
