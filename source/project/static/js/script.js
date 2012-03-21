c56b297c64352c679c4ac48d52477680bf6d6e3c
twidi <s.angel@twidi.com>
2012-03-21 23:29:40 +0100

    Load images in nav *after* the (important) ones in the page. cf #25

--- source/project/static/js/script.js
+++ source/project/static/js/script.js
@@ -1,13 +1,32 @@
 $(document).ready(function(){
 
-    $('li.dropdown.authors li a').each(function() {
-        var a = $(this)
-            img_src = a.data('img-src');
-        if (img_src) {
-            a.prepend($('<img/>').attr('src', img_src));
-        }
-    });
+    var load_logos = function() {
+        $('li.dropdown.authors li a').each(function() {
+            var a = $(this)
+                img_src = a.data('img-src');
+            if (img_src) {
+                a.prepend($('<img/>').attr('src', img_src));
+            }
+        });
+    }
 
+    var important_images = $('#carousel li.item img');
+    if (!$('#carousel').length) {
+        important_images = $('.app-list figure img');
+    }
+    if (important_images.length) {
+        var total_loaded = 0,
+            on_important_img_loaded = function() {
+                total_loaded++;
+                if (total_loaded == important_images.length) {
+                    load_logos();
+                    important_images.unbind('load', on_important_img_loaded);
+                }
+            };
+        important_images.load(on_important_img_loaded);
+    } else {
+        load_logos();
+    }
 
     $('a.app-link').click(function() {
         var link = $(this),
