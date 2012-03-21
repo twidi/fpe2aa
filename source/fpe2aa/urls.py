786f1b61db03155f35644044aecdb9e18e51200e
twidi <s.angel@twidi.com>
2012-03-21 01:50:19 +0100

    Rename "type" into "categories" (in html/url only). Close #20

--- source/fpe2aa/urls.py
+++ source/fpe2aa/urls.py
@@ -5,7 +5,7 @@ from fpe2aa.models import AppType, Platform, Author, Application
 online_applications_queryset = Application.online_only.all()
 
 urlpatterns = patterns('',
-    url(r'^type/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=AppType), name='type_detail'),
+    url(r'^categorie/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=AppType), name='type_detail'),
     url(r'^plateforme/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Platform), name='platform_detail'),
     url(r'^auteur/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Author), name='author_detail'),
     url(r'^applications/$', ListView.as_view(model=Application, queryset=online_applications_queryset), name='application_list'),
