1cb3d4f15802a86e6c97f6ff29344e3b41bb537a
twidi <s.angel@twidi.com>
2012-03-22 10:25:41 +0100

    Load images with adapted size for the carousel (load standard image if no js). cf #25

--- source/project/static/js/script.js
+++ source/project/static/js/script.js
@@ -1,32 +1,106 @@
 $(document).ready(function(){
 
-    var load_logos = function() {
-        $('li.dropdown.authors li a').each(function() {
-            var a = $(this)
-                img_src = a.data('img-src');
-            if (img_src) {
-                a.prepend($('<img/>').attr('src', img_src));
-            }
-        });
-    }
 
-    var important_images = $('#carousel li.item img');
-    if (!$('#carousel').length) {
-        important_images = $('.app-list figure img');
-    }
-    if (important_images.length) {
-        var total_loaded = 0,
-            on_important_img_loaded = function() {
-                total_loaded++;
-                if (total_loaded == important_images.length) {
-                    load_logos();
-                    important_images.unbind('load', on_important_img_loaded);
+    var carousel = $('#carousel');
+
+    var important_images = {
+        container: carousel.length ? carousel : $('.app-list')
+    };
+    important_images = $.extend({}, important_images, {
+        loaded: 0,
+        total: important_images.container.find('figure').length,
+
+        load_logos: function() {
+            /* Load all logos tkong data from the surrouding a */
+            $('li.dropdown.authors li a').each(function() {
+                var a = $(this)
+                    img_src = a.data('img-src');
+                if (img_src) {
+                    a.prepend($('<img/>').attr('src', img_src));
                 }
-            };
-        important_images.load(on_important_img_loaded);
-    } else {
-        load_logos();
-    }
+            });
+        },
+
+        on_img_loaded: function() {
+            /* Load logos when the last important image is loaded */
+            important_images.loaded++;
+            if (important_images.loaded == important_images.total) {
+                important_images.load_logos();
+                $(this).unbind('load', important_images.on_img_loaded);
+            }
+        },
+
+        carousel: {
+            current_width: 440,
+            widths: [440, 538, 712, 870],
+
+            resized: function(first_run) {
+                /* Use images adapted to the carousel width (so, the screen width) */
+                var new_width = null,
+                    test_width = $('#carousel').innerWidth();
+                // look for maximal matching width
+                for (var i = 0; i < important_images.carousel.widths.length; i += 1) {
+                    if (test_width <= important_images.carousel.widths[i]) {
+                        new_width = important_images.carousel.widths[i];
+                        break;
+                    }
+                };
+                // if new width of first run (which will replace placeholder with an image)
+                if (first_run || (new_width && new_width != important_images.carousel.current_width)) {
+                    var old_current_width = important_images.carousel.current_width;
+                    important_images.carousel.current_width = new_width;
+                    var all =  carousel.find('figure img, figure b');
+                    var update_src = function(node, on_load) {
+                        var is_img = !!(node.get(0).nodeName == 'IMG'),
+                            src = is_img ? node.attr('src') : node.data('img-src'),
+                            new_src = src.replace(old_current_width + 'x0_q85', new_width + 'x0_q85');
+                        if (is_img && src == new_src) { return; }
+                        if (!is_img) {
+                            var img = $('<img />').attr('alt', node.data('img-alt')).attr('src', new_src);
+                            if (on_load) {
+                                img.load(on_load);
+                            }
+                            img.load(important_images.on_img_loaded);
+                            node.parent().prepend(img);
+                            node.remove();
+                        } else {
+                            if (on_load) {
+                                node.load(on_load);
+                            }
+                            node.attr('src', new_src);
+                        }
+                    };
+                    var on_first_loaded = function() {
+                        $(this).unbind('load', on_first_loaded);
+                        carousel.find('figure img, figure span').each(function() {
+                            update_src($(this));
+                        });
+                    };
+                    update_src(carousel.find('figure img, figure span').eq(0), on_first_loaded);
+                }
+            },
+
+            run: function() {
+                important_images.carousel.resized(true);
+                var on_win_resize = function() { important_images.carousel.resized(false) }
+                $(window).resize(on_win_resize);
+            }
+
+        }, // carousel
+
+        run: function() {
+            if (important_images.total) {
+                important_images.container.find('figure img').load(important_images.on_img_loaded);
+            } else {
+                important_load_logos();
+            }
+            if (carousel.length) {
+                important_images.carousel.run();
+            }
+        }
+    }); // important_images
+
+    important_images.run();
 
     $('a.app-link').click(function() {
         var link = $(this),
