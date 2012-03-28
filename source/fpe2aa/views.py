twidi/fpe2aa

commit 760c97d66caeed4f15989c82139bb7aa75b615d7

Add sort options. cf #29

source/fpe2aa/views.py
@@ -1,1 +1,62 @@
-# Create your views here.
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+
+from fpe2aa.models import Application, Author, AppType, Platform
+
+class ApplicationsView(ListView):
+    context_object_name = 'applications'
+    model = Application
+
+    def get_queryset(self):
+        qs = self.model.online_only.all()
+
+        self._sort = self.request.GET.get('sort', '')
+        if self._sort == 'name':
+            qs = qs.order_by('slug')
+        elif self._sort == 'date':
+            qs = qs.order_by('-date_add')
+        else:
+            self._sort = None
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(ApplicationsView, self).get_context_data(**kwargs)
+        context['sort'] = self._sort
+        return context
+
+class AuthorView(ApplicationsView):
+
+    def get_queryset(self):
+        self._author = get_object_or_404(Author, slug=self.kwargs['slug'])
+        qs = super(AuthorView, self).get_queryset()
+        return qs.filter(authors=self._author)
+
+    def get_context_data(self, **kwargs):
+        context = super(AuthorView, self).get_context_data(**kwargs)
+        context['author'] = self._author
+        return context
+
+class AppTypeView(ApplicationsView):
+
+    def get_queryset(self):
+        self._apptype = get_object_or_404(AppType, slug=self.kwargs['slug'])
+        qs = super(AppTypeView, self).get_queryset()
+        return qs.filter(apptypes=self._author)
+
+    def get_context_data(self, **kwargs):
+        context = super(AppTypeView, self).get_context_data(**kwargs)
+        context['type'] = self._apptype
+        return context
+
+class PlatformView(ApplicationsView):
+
+    def get_queryset(self):
+        self._platform = get_object_or_404(Platform, slug=self.kwargs['slug'])
+        qs = super(PlatformView, self).get_queryset()
+        return qs.filter(platforms=self._platform)
+
+    def get_context_data(self, **kwargs):
+        context = super(PlatformView, self).get_context_data(**kwargs)
+        context['platform'] = self._platform
+        return context

source/fpe2aa/templates/fpe2aa/apptype_detail.html
@@ -1,14 +1,14 @@
 {% extends "base.html" %}
 
-{% block title %}{{ object }} - {{ block.super }}{% endblock %}
-{% block description %}Applications présidentielle de type {{ object }}{% endblock %}
+{% block title %}{{ type }} - {{ block.super }}{% endblock %}
+{% block description %}Applications présidentielle de type {{ type }}{% endblock %}
 
 {% block header %}
 <section class="well">
-<h1>Applications de type <em>{{ object }}</em></h1>
+    <h1>Applications de type <em>{{ type }}</em></h1>
 </section>
 {% endblock %}
 
 {% block main %}
-    {% include "fpe2aa/includes/applications.html" with applications=object.online_applications %}
+    {% include "fpe2aa/includes/applications.html" %}
 {% endblock main %}

source/fpe2aa/templates/fpe2aa/author_detail.html
@@ -1,14 +1,14 @@
 {% extends "base.html" %}
 
-{% block title %}{{ object }} - {{ block.super }}{% endblock %}
-{% block description %}Applications présidentielle de {{ object }}{% endblock %}
+{% block title %}{{ author }} - {{ block.super }}{% endblock %}
+{% block description %}Applications présidentielle de {{ author }}{% endblock %}
 
 {% block header %}
 <section class="well">
-<h1>Applications de <em>{{ object }}</em>{% if author.logo %} <a href="{{ object.link }}" target="_blank"><img src="{{ author.logo.url }}" /></a>{% endif %}</h1>
-    </section>
+    <h1>Applications de <em>{{ author }}</em>{% if author.logo %} <a href="{{ author.link }}" target="_blank"><img src="{{ author.logo.url }}" /></a>{% endif %}</h1>
+</section>
 {% endblock %}
 
 {% block main %}
-    {% include "fpe2aa/includes/applications.html" with applications=object.online_applications %}
+    {% include "fpe2aa/includes/applications.html" %}
 {% endblock main %}

source/fpe2aa/templates/fpe2aa/includes/applications.html
@@ -1,3 +1,23 @@
+<nav id="sort" class="btn-group">
+    <a class="btn dropdown-toggle" data-toggle="dropdown">
+            Tri
+            <small>
+            {% if sort == 'name' %}
+                (nom)
+            {% elif sort == 'date' %}
+                (date)
+            {% else %}
+                (popularité)
+            {% endif %}
+            </small>
+            <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu pull-right">
+        <li{% if sort != "name" and sort != "date" %} class="active"{% endif %}><a href=".">Popularité</a></li>
+        <li{% if sort == "name" %} class="active"{% endif %}><a href=".?sort=name">Nom</a></li>
+        <li{% if sort == "date" %} class="active"{% endif %}><a href=".?sort=date">Date d'ajout</a></li>
+    </ul>
+</nav>
 <section class="app-list row">
     {% for application in applications %}
         {% if forloop.first or forloop.counter0|divisibleby:4 %}

source/fpe2aa/templates/fpe2aa/platform_detail.html
@@ -1,14 +1,14 @@
 {% extends "base.html" %}
 
-{% block title %}{{ object }} - {{ block.super }}{% endblock %}
-{% block description %}Applications présidentielle de la plate-forme {{ object }}{% endblock %}
+{% block title %}{{ platform }} - {{ block.super }}{% endblock %}
+{% block description %}Applications présidentielle de la plate-forme {{ platform }}{% endblock %}
 
 {% block header %}
 <section class="well">
-    <h1>Applications de la plate-forme <em>{{ object }}</em></h1>
+    <h1>Applications de la plate-forme <em>{{ platform }}</em></h1>
 </section>
 {% endblock %}
 
 {% block main %}
-    {% include "fpe2aa/includes/applications.html" with applications=object.online_applications %}
+    {% include "fpe2aa/includes/applications.html" %}
 {% endblock main %}

source/fpe2aa/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView, DetailView
-from fpe2aa.models import AppType, Platform, Author, Application
-
-online_applications_queryset = Application.online_only.all()
+from fpe2aa.views import AppTypeView, AuthorView, PlatformView, ApplicationsView
 
 urlpatterns = patterns('',
-    url(r'^categorie/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=AppType), name='type_detail'),
-    url(r'^plateforme/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Platform), name='platform_detail'),
-    url(r'^auteur/(?P<slug>[\w\/\-]+)/$', DetailView.as_view(model=Author), name='author_detail'),
-    url(r'^applications/$', ListView.as_view(model=Application, queryset=online_applications_queryset), name='application_list'),
+    url(r'^categorie/(?P<slug>[\w\/\-]+)/$', AppTypeView.as_view(), name='type_detail'),
+    url(r'^plateforme/(?P<slug>[\w\/\-]+)/$', PlatformView.as_view(), name='platform_detail'),
+    url(r'^auteur/(?P<slug>[\w\/\-]+)/$', AuthorView.as_view(), name='author_detail'),
+    url(r'^applications/$', ApplicationsView.as_view(), name='application_list'),
 )
 

source/project/static/css/app.css
@@ -285,7 +285,16 @@ html.no-js .carousel-control-container {
   font-size: 13px;
   color: #333;
 }
+#sort {
+  float: right;
+  margin-top: -55px;
+  margin-right: 8px;
+}
+html.no-js #sort {
+  display: none;
+}
 .app-row {
+  clear: both;
   vertical-align: top;
 }
 .app-row > li {

source/project/static/css/app.min.css
@@ -34,7 +34,9 @@ html.no-js .carousel-control-container{display:none;}
 .carousel-caption a{color:white;}
 .carousel-caption footer{border-top:solid #666 1px;padding-top:5px;}.carousel-caption footer a{text-decoration:underline;}
 .carousel-caption .btn{font-size:13px;color:#333;}
-.app-row{vertical-align:top;}.app-row>li{-webkit-box-shadow:4px 4px 8px rgba(0, 0, 0, 0.5);-moz-box-shadow:4px 4px 8px rgba(0, 0, 0, 0.5);box-shadow:4px 4px 8px rgba(0, 0, 0, 0.5);position:relative;display:inline-block;float:none;vertical-align:top;padding:5px;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;-ms-box-sizing:border-box;box-sizing:border-box;background:black;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;margin-bottom:20px;}.app-row>li img{max-width:100%;}
+#sort{float:right;margin-top:-55px;margin-right:8px;}
+html.no-js #sort{display:none;}
+.app-row{clear:both;vertical-align:top;}.app-row>li{-webkit-box-shadow:4px 4px 8px rgba(0, 0, 0, 0.5);-moz-box-shadow:4px 4px 8px rgba(0, 0, 0, 0.5);box-shadow:4px 4px 8px rgba(0, 0, 0, 0.5);position:relative;display:inline-block;float:none;vertical-align:top;padding:5px;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;-ms-box-sizing:border-box;box-sizing:border-box;background:black;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;margin-bottom:20px;}.app-row>li img{max-width:100%;}
 .app-row>li article{padding:5px 10px;-webkit-border-radius:0 0 5px 5px;-moz-border-radius:0 0 5px 5px;border-radius:0 0 5px 5px;}.app-row>li article h4{margin-bottom:0;}
 .app-row>li article span a{text-decoration:none;}
 .app-row>li article section,.app-row>li article footer{display:none;}

commit 204cb4418a47f9a81675381909cf530d7afaf184

Correct a bug introduced in [760c97d6] (cf #29)

source/fpe2aa/views.py
@@ -42,7 +42,7 @@ class AppTypeView(ApplicationsView):
     def get_queryset(self):
         self._apptype = get_object_or_404(AppType, slug=self.kwargs['slug'])
         qs = super(AppTypeView, self).get_queryset()
-        return qs.filter(apptypes=self._author)
+        return qs.filter(types=self._apptype)
 
     def get_context_data(self, **kwargs):
         context = super(AppTypeView, self).get_context_data(**kwargs)